c5741f6422ca3149a6247c6ce3d17b901e793b5c
VanPhamNe <vanliken1@gmail.com>
2023-11-15 13:44:23 +0700

    title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         van ne
         </p>
         <a
           className="App-link"
